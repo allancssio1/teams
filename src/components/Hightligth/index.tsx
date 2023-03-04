fd45ec453ab24287391e3fb1cf908dd59dfca524
allancassio <allan.cassio1@gmail.com>
2023-03-04 14:17:22 -0300

    created component with title and subtitle

---
+++ src/components/Hightligth/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container, Subtitle, Title } from "./styles";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export function Hightligth({ subtitle, title }: Props) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+    </Container>
+  );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
+import { Hightligth } from "@components/Hightligth";
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Hightligth title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
