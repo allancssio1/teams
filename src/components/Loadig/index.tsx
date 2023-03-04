5140f7814ad3de241e2083b18f282ad1b3bd9075
allancassio <allan.cassio1@gmail.com>
2023-03-04 14:01:38 -0300

    first commit

---
+++ src/components/Loadig/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Loading } from "./styles";
+
+export function Loadig() {
+  return (
+    <Container>
+      <Loading />
+    </Container>
+  );
+}
